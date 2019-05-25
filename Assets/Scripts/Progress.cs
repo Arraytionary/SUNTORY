97c54b04d0ab3c8d4e605a8739a740ac67c22362
Arraytionary <31273055+Arraytionary@users.noreply.github.com>
2019-05-25 22:52:42 +0700

    add character

---
+++ Assets/Scripts/Progress.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+public class Progress : Singleton<Progress> 
+{
+    // public int[] test;
+    public IDictionary<string, int> dict = new Dictionary<string, int>(){
+        {"demo", 1}
+    };
+    // dict.Add();
+    // int res;
+    // dict.TryGetValue("demo", out res);
+}
