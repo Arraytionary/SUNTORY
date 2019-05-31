ee91f7e6ba51681194a8626d4438d8c6a7b9fce8
Arraytionary <31273055+Arraytionary@users.noreply.github.com>
2019-06-01 00:40:56 +0700

    Added start menu

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    // public Button startButton;
+    // public Button LoadButton;
+    public void startGame(){
+        Progress.nextScene(0);
+    }
+    public void loadGame(){
+        Debug.Log("load game");
+    }
+}
